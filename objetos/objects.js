7e6b4fe5a00340df9357d9f19b249b3ffefe9bff
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-21 15:42:49 -0500

    agrega objets.ts

---
+++ objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super fuerza']
+    };
+})();
---
+++ objetos/objects.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    let flash = {
+        name: 'Barry Allen',
+        age:24,
+        powers:['Super Velocidad','Viajar en el tiempo']
+    }
+
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers:['Super fuerza']
+    }
+})()
