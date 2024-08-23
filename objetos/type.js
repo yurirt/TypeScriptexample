b992483e7faae3dd28145a6e1d17b7eb54fad9ae
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-23 09:53:21 -0500

    agrega ejemplo type.ts

---
+++ objetos/type.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    };
+    let superMan = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super Velocidad', 'Viajar en el tiempo'],
+        getName() {
+            return this.name;
+        }
+    };
+})();
