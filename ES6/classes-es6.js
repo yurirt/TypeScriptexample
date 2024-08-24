a84164163dcdc3d25fd3e39f641000748db711bc
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-24 12:31:39 -0500

    example class in JS

---
+++ ES6/classes-es6.js
@@ -0,0 +1,29 @@
+(() => {
+
+    class Avenger {
+        name;
+        power;
+
+        constructor(name='No name', power = 0) {
+            this.name = name;
+            this.power = power;
+        }
+    }
+
+
+    class FlyingAvenger extends Avenger {
+        flying;
+
+        constructor(name, power) {
+            super(name, power);
+            this.flying = true;
+        }
+    };
+
+    const hulk = new Avenger('hulk',9000);
+    const falcon = new FlyingAvenger('Falcon',50);
+
+    console.log(hulk);
+    console.log(falcon);
+
+})()
