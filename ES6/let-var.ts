6f01871ffe253af7db22f4fb9c5feec1cf8e33e9
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-24 11:17:06 -0500

    agregar configuracion para compilacion en include

---
+++ ES6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+
+    // console.log('let');
+    const nombre = 'Yuri'
+
+    const getName = () => {
+        console.log('viejo getName');
+    }
+
+    // getName = () => {console.log('Nuevo getName')};
+
+})()
