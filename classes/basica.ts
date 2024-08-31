1f8598edbfcc032cb81076fa03346a3de5f16a45
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-31 10:59:41 -0500

    metodos

--- classes/basica.ts
+++ classes/basica.ts
@@ -6,19 +6,24 @@
         // public team: string;
         // public realName?: string;
         static avgAge:number = 35;
+        static getAvgAge(){
+            return this.name;
+        }
 
         constructor(private name:string, 
                     private team: string, 
                     public realName?: string,
                     avgAge:number = 5
-                ) {
-                    Avenger.avgAge = avgAge;
-                }
+                ) {}
+        public bio(){
+            return `${ this.name } (${ this.team })`
+        }
     }
 
     const antman : Avenger = new Avenger('Antman','Capitan');
 
     console.log( antman );
-    console.log( Avenger.avgAge );
+    console.log( antman.bio() );
+    console.log( Avenger.getAvgAge() );
 
 })()
--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName, avgAge = 5) {
             this.name = name;
             this.team = team;
             this.realName = realName;
-            Avenger.avgAge = avgAge;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
         }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan');
     console.log(antman);
-    console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
