d248547ef3ee0b5a66daa7399ddcc3ce5746cfca
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-08-31 10:39:49 -0500

    clases constructor

--- classes/basica.ts
+++ classes/basica.ts
@@ -2,20 +2,23 @@
 
     class Avenger {
 
-        private name: string;
-        public team: string;
-        public realName?: string;
+        // private name: string = 'Scott';
+        // public team: string;
+        // public realName?: string;
         static avgAge:number = 35;
 
-        constructor(name:string, team: string, realName?: string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
+        constructor(private name:string, 
+                    private team: string, 
+                    public realName?: string,
+                    avgAge:number = 5
+                ) {
+                    Avenger.avgAge = avgAge;
+                }
     }
 
     const antman : Avenger = new Avenger('Antman','Capitan');
 
     console.log( antman );
+    console.log( Avenger.avgAge );
 
 })()
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, team, realName, avgAge = 5) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+            Avenger.avgAge = avgAge;
         }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan');
     console.log(antman);
+    console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
