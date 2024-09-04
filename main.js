fd552535b36a77e8d0618a1a25eb8ed208ffa927
Yuri Rojas Trujillo <yurirt.2@gmail.com>
2024-09-03 23:27:31 -0500

    namespace

--- main.js
+++ main.js
@@ -1,61 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super Velocidad', 'Viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superMan = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super Velocidad', 'Viajar en el tiempo'],
-        getName() {
-            return this.name;
-        }
+    const validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor() {
-            this.age = 0;
-            this.name = 'Sin Nombre';
-            this.realName = 'Sin Nombre real';
-        }
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Fernando',
-        age: 25,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFullAdress() {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Melissa',
-        age: 30,
-        address: {
-            city: 'Toronto',
-            id: 120,
-            zip: 'K2S U2A'
-        },
-        getFullAdress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addTwoNumbersFuction;
-    addTwoNumbersFuction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('yuri'));
 //# sourceMappingURL=main.js.map
